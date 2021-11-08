9f830a466146d5e6297144b1d533632fc8c532d0
chernykh-ru <86768486+chernykh-ru@users.noreply.github.com>
2021-11-08 12:02:15 +0300

    add follow unfollow api

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -2,6 +2,7 @@ import styles from './Users.module.css';
 import avataaars from './../../../src/assets/images/avataaars.png';
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import axios from 'axios';
 
 const Users = (props) => {
   let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
@@ -45,14 +46,36 @@ const Users = (props) => {
               {user.followed ? (
                 <button
                   onClick={() => {
-                    props.unfollow(user.id); //по клику отрабатываем колбэк, берем в пропсах функцию unfollow(вызываем unfollow из контейн комп и передаем туда id)
+                    axios
+                      .delete(`https://social-network.samuraijs.com/api/1.0/follow/${user.id}`, {
+                        withCredentials: true,
+                        headers: { 'API-KEY': '6e65516f-e614-437d-93bf-9e85e40713b5' }, //DELETE(GET) second params options withCred(true) and headers{api-key}
+                      })
+                      .then((response) => {
+                        if (response.data.resultCode === 0) {
+                          props.unfollow(user.id);
+                        } //если отписка произошла и сервер подтвердил(resultCode === 0) диспачим в редьюсер
+                      });
                   }}>
                   Unfollow
                 </button>
               ) : (
                 <button
                   onClick={() => {
-                    props.follow(user.id); //в целом если юзер followed(true) подставляем ему кнопку unfollow и наоборот(тернарный)
+                    axios
+                      .post(
+                        `https://social-network.samuraijs.com/api/1.0/follow/${user.id}`,
+                        {}, //POST second params null or empty obj
+                        {
+                          withCredentials: true,
+                          headers: { 'API-KEY': '6e65516f-e614-437d-93bf-9e85e40713b5' },
+                        }, //POST(PUT) third params options withCred(true) and headers{api-key}
+                      )
+                      .then((response) => {
+                        if (response.data.resultCode === 0) {
+                          props.follow(user.id);
+                        } //если подписка произошла и сервер подтвердил(resultCode === 0) диспачим в редьюсер
+                      });
                   }}>
                   Follow
                 </button>
--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -25,6 +25,9 @@ class UsersContainer extends React.Component {
     axios
       .get(
         `https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`,
+        {
+          withCredentials: true,
+        }, //так же проверяем что пользователь залогинен и цепляем куки
       )
       .then((response) => {
         this.props.setUsers(response.data.items); //получаем user из response(ответ) data(данные) items(объект с юзерами) и диспачим setUsers
@@ -39,6 +42,9 @@ class UsersContainer extends React.Component {
     axios
       .get(
         `https://social-network.samuraijs.com/api/1.0/users?page=${currentPage}&count=${this.props.pageSize}`,
+        {
+          withCredentials: true,
+        },
       )
       .then((res) => {
         this.props.toggleIsFetching(false); //запрос пришел preloader скрывается
