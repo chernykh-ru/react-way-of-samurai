840ea7ee8706754aaa09666661411f71d3a9e134
chernykh-ru <86768486+chernykh-ru@users.noreply.github.com>
2021-11-19 15:09:17 +0300

    making a separate user component

---
+++ src/components/Users/User.jsx
@@ -0,0 +1,52 @@
+import styles from './Users.module.css';
+import avataaars from './../../../src/assets/images/avataaars.png';
+import { NavLink } from 'react-router-dom';
+
+const User = ({ followingInProgress, follow, unfollow, user }) => {
+  return (
+    <div>
+      <div>
+        <div>
+          <NavLink to={'/profile/' + user.id}>
+            <img
+              className={styles.userPhoto}
+              src={user.photos.small != null ? user.photos.small : avataaars} //добавляем проверку, если с сервера не пришла аватарка, поставить заглушку
+              alt='avatar'
+            />
+          </NavLink>
+        </div>
+        <div>
+          {user.followed ? (
+            <button
+              disabled={followingInProgress.some((id) => id === user.id)}
+              onClick={() => {
+                unfollow(user.id);
+              }}>
+              Unfollow
+            </button>
+          ) : (
+            <button
+              disabled={followingInProgress.some((id) => id === user.id)}
+              onClick={() => {
+                follow(user.id);
+              }}>
+              Follow
+            </button>
+          )}
+        </div>
+      </div>
+      <div className={styles.wrapper__item}>
+        <div>
+          <div>{user.name}</div>
+          <div>{user.status}</div>
+        </div>
+        <div>
+          {/* <div>{user.location.country}</div>
+                <div>{user.location.city}</div> */}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default User;
