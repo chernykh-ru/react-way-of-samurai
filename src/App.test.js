a7105ec8a02070546a87c112d6364f5b101d7720
chernykh-ru <86768486+chernykh-ru@users.noreply.github.com>
2021-11-19 08:33:36 +0300

    add jest test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,25 @@
-import { render, screen } from '@testing-library/react';
+// import { render, screen } from '@testing-library/react';
 import App from './App';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/redux-store';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(
+    <Router>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </Router>,
+    div,
+  );
+  ReactDOM.unmountComponentAtNode(div);
 });
