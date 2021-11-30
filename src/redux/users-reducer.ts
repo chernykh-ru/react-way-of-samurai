280679327f11be62f04413bab21deeed732c1960
chernykh-ru <86768486+chernykh-ru@users.noreply.github.com>
2021-11-30 15:39:02 +0300

    refactoring thunks to typescript

--- src/redux/users-reducer.ts
+++ src/redux/users-reducer.ts
@@ -1,6 +1,9 @@
 import { usersAPI } from '../api/api';
 import { updateObjectInArray } from '../utils/object-helpers';
 import {PhotosType, UsersType} from '../types/types'
+import { AppStateType } from './redux-store';
+import { Dispatch } from 'redux';
+import { ThunkAction } from 'redux-thunk'
 
 const FOLLOW = 'WAY-OF-SAMURAI/USERS/FOLLOW'; //add redux-ducks
 const UNFOLLOW = 'WAY-OF-SAMURAI/USERS/UNFOLLOW';
@@ -9,21 +12,6 @@ const SET_CURRENT_PAGE = 'WAY-OF-SAMURAI/USERS/SET_CURRENT_PAGE';
 const SET_TOTAL_USERS_COUNT = 'WAY-OF-SAMURAI/USERS/SET_TOTAL_USERS_COUNT';
 const TOGGLE_IS_FETCHING = 'WAY-OF-SAMURAI/USERS/TOGGLE_IS_FETCHING';
 const TOGGLE_IS_FOLLOWING_PROGRESS = 'WAY-OF-SAMURAI/USERS/TOGGLE_IS_FOLLOWING_PROGRESS';
-
-//выносим типы в отдельный файл types и импортируем их по необходимости
-// type PhotosType = {
-//   small: string | null,
-//   large: string | null,
-// }
-
-// type UsersType = {
-//   name: string | null,
-//   id: number | null,
-//   uniqueUrlName:null,
-//   photos: PhotosType,
-//   status: string | null,
-//   followed: boolean,
-// }
 
 let initialState = {
   users: [] as Array<UsersType>,
@@ -36,7 +24,7 @@ let initialState = {
 
 export type InitialStateType = typeof initialState
 
-const usersReducer = (state: InitialStateType = initialState, action: FollowSuccessActionType | UnfollowSuccessActionType | SetUsersActionType | SetCurrentPageActionType | SetTotalUsersCountActionType | ToggleIsFetchingActionType | ToggleFollowingProgressActionType): InitialStateType => {
+const usersReducer = (state: InitialStateType = initialState, action: ActionsTypes): InitialStateType => {
   switch (action.type) {
     //======
     //рефакторинг, вынесение дублирующей логики из follow и unfollow во вспомогутальную функцию updateObjectInArray из утилит
@@ -47,7 +35,7 @@ const usersReducer = (state: InitialStateType = initialState, action: FollowSucc
       };
     case UNFOLLOW:
       return {
-        ...state, //создаем копию стейта(работать можем только с копией)
+        ...state,
         users: updateObjectInArray(state.users, action.userId, 'id', { followed: false }),
       };
     //рефакторинг, вынесение дублирующей логики из follow и unfollow во вспомогутальную функцию updateObjectInArray из утилит
@@ -112,6 +100,8 @@ const usersReducer = (state: InitialStateType = initialState, action: FollowSucc
 };
 
 //ACTypes
+type ActionsTypes = FollowSuccessActionType | UnfollowSuccessActionType | SetUsersActionType | SetCurrentPageActionType | SetTotalUsersCountActionType | ToggleIsFetchingActionType | ToggleFollowingProgressActionType
+
 type FollowSuccessActionType = {
   type: typeof FOLLOW,
   userId: number | null
@@ -153,10 +143,6 @@ export const setTotalUsersCount = (totalUsersCount: number): SetTotalUsersCountA
   type: SET_TOTAL_USERS_COUNT,
   totalUsersCount, //когда ключ и значение не одинаковы(специально)
 });//remove | null
-// export const setTotalUsersCount = (totalUsersCount: number | null) => ({
-//   type: SET_TOTAL_USERS_COUNT,
-//   count: totalUsersCount, //когда ключ и значение не одинаковы(специально)
-// });
 export const toggleIsFetching = (isFetching: boolean): ToggleIsFetchingActionType => ({
   type: TOGGLE_IS_FETCHING,
   isFetching,
@@ -168,8 +154,11 @@ export const toggleFollowingProgress = (isFetching: boolean, userId: number | nu
 });
 
 //создаем функции thunk creator, которая создает(возвращает) thunk(dispatch action)
-//переименовываем AC без окончания AC
-export const requestUsers = (page: number, pageSize: number | null) => async (dispatch: any) => {
+type GetStateType = () => AppStateType//создаем "псевдоним" типа для getState
+type DispatchType = Dispatch<ActionsTypes>//создаем "псевдоним" типа для dispatch
+type ThunkType = ThunkAction<Promise<void>, AppStateType, unknown, ActionsTypes>
+
+export const requestUsers = (page: number, pageSize: number | null): ThunkAction<Promise<void>, AppStateType, unknown, ActionsTypes> => async (dispatch, getState) => {
   dispatch(setCurrentPage(page)); //from onPageChanged(активная страница)
   dispatch(toggleIsFetching(true)); //запрос ушел preloader виден
   const data = await usersAPI.getUsers(page, pageSize);
@@ -178,21 +167,8 @@ export const requestUsers = (page: number, pageSize: number | null) => async (di
   dispatch(setTotalUsersCount(data.totalCount));
 }; //remove page | null
 
-// export const requestUsers = (page, pageSize) => {
-//   // debugger;
-//   return (dispatch) => {
-//     dispatch(setCurrentPage(page)); //from onPageChanged(активная страница)
-//     dispatch(toggleIsFetching(true)); //запрос ушел preloader виден
-//     usersAPI.getUsers(page, pageSize).then((data) => {
-//       dispatch(toggleIsFetching(false)); //запрос пришел preloader скрывается
-//       dispatch(setUsers(data.items)); // у usersAPI дергаем метод getUsers//получаем user из data(данные) items(объект с юзерами) и диспачим setUsers
-//       dispatch(setTotalUsersCount(data.totalCount));
-//     });
-//   };
-// };//with then
-
 //рефакторинг, вынесение дублирующей логики из follow и unfollow в функцию followUnfollowFlow
-export const followUnfollowFlow = async (dispatch: any, userId: number | null, apiMethod: any, actionCreator: any) => {
+const _followUnfollowFlow = async (dispatch: DispatchType, userId: number | null, apiMethod: any, actionCreator: (userId: number | null) => FollowSuccessActionType | UnfollowSuccessActionType) => {
   dispatch(toggleFollowingProgress(true, userId));
   const data = await apiMethod(userId);
   if (data.resultCode === 0) {
@@ -201,30 +177,12 @@ export const followUnfollowFlow = async (dispatch: any, userId: number | null, a
   dispatch(toggleFollowingProgress(false, userId));
 };
 
-export const follow = (userId: number | null) => async (dispatch: any) => {
-  // const apiMethod = usersAPI.follow.bind(usersAPI);
-  // const actionCreator = followSuccess;
-  followUnfollowFlow(dispatch, userId, usersAPI.follow.bind(usersAPI), followSuccess);
+export const follow = (userId: number | null): ThunkType => async (dispatch) => {
+  _followUnfollowFlow(dispatch, userId, usersAPI.follow.bind(usersAPI), followSuccess);
+};//типизируем TC по типу ThunkType
 
-  // dispatch(toggleFollowingProgress(true, userId));
-  // const data = await apiMethod(userId);
-  // if (data.resultCode === 0) {
-  //   dispatch(actionCreator(userId));
-  // } //если подписка произошла и сервер подтвердил(resultCode === 0) диспачим в редьюсер
-  // dispatch(toggleFollowingProgress(false, userId));
-}; //convert to async/await
-
-export const unfollow = (userId: number | null) => async (dispatch: any) => {
-  // const apiMethod = usersAPI.unfollow.bind(usersAPI);
-  // const actionCreator = unfollowSuccess;
-  followUnfollowFlow(dispatch, userId, usersAPI.unfollow.bind(usersAPI), unfollowSuccess);
-
-  // dispatch(toggleFollowingProgress(true, userId));
-  // const data = await apiMethod(userId);
-  // if (data.resultCode === 0) {
-  //   dispatch(actionCreator(userId));
-  // } //если отписка произошла и сервер подтвердил(resultCode === 0) диспачим в редьюсер
-  // dispatch(toggleFollowingProgress(false, userId));
-}; //convert to async/await
+export const unfollow = (userId: number | null): ThunkType => async (dispatch) => {
+  _followUnfollowFlow(dispatch, userId, usersAPI.unfollow.bind(usersAPI), unfollowSuccess);
+};
 
 export default usersReducer;
