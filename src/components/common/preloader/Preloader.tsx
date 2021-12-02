f602f6687c85d95ecbd6080808b20f14e81de370
chernykh-ru <86768486+chernykh-ru@users.noreply.github.com>
2021-12-02 17:46:58 +0300

    cleanup old JS-components

--- src/components/common/preloader/Preloader.tsx
+++ src/components/common/preloader/Preloader.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from '../../Users/Users.module.css';
 import preloader from '../../../assets/images/preloader.svg';
 
-const Preloader = (props) => {
+const Preloader: React.FC = () => {
   return (
     <>
       <img className={styles.preloader} src={preloader} alt='is loading...' />
