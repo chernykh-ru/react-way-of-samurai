1791ae581f1afd892964fe0285ce27449d9576d1
chernykh-ru <86768486+chernykh-ru@users.noreply.github.com>
2021-12-14 16:41:59 +0300

    additional lecture on useEffect (sandbox)

--- src/pages/Sandbox/Github.tsx
+++ src/pages/Sandbox/Github.tsx
@@ -1,30 +1,40 @@
 import styles from './Sandbox.module.css';
 import { useEffect, useState } from 'react';
-import { Row, Col, Divider, Button, Input } from 'antd';
-import { Typography } from 'antd';
+import { Row, Col, Divider, Button, Input, Avatar, Typography, Anchor } from 'antd';
 import axios from 'axios';
 
-type SearchUsertype = {
+const { Link } = Typography;
+
+type SearchUserType = {
   login: string;
   id: number;
 };
 
+type SelectedUserType = {
+  login: string;
+  id: number;
+  avatar_url: string;
+  html_url: string;
+  followers: string;
+};
+
 type SearchResult = {
-  items: SearchUsertype[];
+  items: SearchUserType[];
 };
 
 export const Github = () => {
   console.log('rendering github');
   const { Title } = Typography;
-  const [selectedUser, setSelectedUser] = useState<SearchUsertype | null>(null);
-  const [users, setUsers] = useState<SearchUsertype[]>([]); //локальное хранилище юзеров
+  const [selectedUser, setSelectedUser] = useState<SearchUserType | null>(null);
+  const [users, setUsers] = useState<SearchUserType[]>([]); //локальное хранилище юзеров
   const [tempSearch, setTempSearch] = useState('chernykh'); //input value
   const [termSearch, setTermSearch] = useState('chernykh'); //value for fetch
+  const [selectedUserDetails, setSelectedUsersDetails] = useState<SelectedUserType | null>(null); //локальное хранилище профиля юзера
 
-  useEffect(() => {
-    console.log('sync title');
-    if (selectedUser) document.title = selectedUser.login;
-  }, [selectedUser]);
+  // useEffect(() => {
+  //   console.log('sync title');
+  //   if (selectedUser) document.title = selectedUser.login;
+  // }, [selectedUser]);
 
   // const fetchData = (term: string) => {
   //   axios.get<SearchResult>(`https://api.github.com/search/users?q=${term}`).then((res) => {
@@ -38,6 +48,19 @@ export const Github = () => {
       setUsers(res.data.items); //сетаем полученных юзеров в локальный стейт
     });
   }, [termSearch]); //засисимость от синхронизированого стейта с полем ввода(по кнопке)
+  // console.log(users);
+
+  useEffect(() => {
+    // console.log('sync users from api');
+    if (selectedUser) document.title = selectedUser.login;
+    // console.log(selectedUser?.login);
+    if (!!selectedUser)
+      axios
+        .get<SelectedUserType>(`https://api.github.com/users/${selectedUser.login}`)
+        .then((res) => {
+          setSelectedUsersDetails(res.data); //сетаем полученного юзера в локальный стейт
+        });
+  }, [selectedUser]); //засисимость от выбранного юзера
   // console.log(users);
 
   return (
@@ -71,10 +94,19 @@ export const Github = () => {
             ))}
           </ul>
         </Col>
-        <Col span={8} offset={2}>
-          <Title level={2}>Username</Title>
-          <div>Details</div>
-        </Col>
+        {selectedUserDetails && (
+          <Col span={8} offset={2}>
+            <Link href={selectedUserDetails.html_url}>
+              <Title level={2}>{selectedUserDetails.login}</Title>
+              <Avatar
+                src={selectedUserDetails.avatar_url}
+                size={{ sm: 40, md: 60, lg: 84, xl: 160 }}
+              />
+            </Link>
+            <p>User id: {selectedUserDetails.id}</p>
+            <p>Followers: {selectedUserDetails.followers}</p>
+          </Col>
+        )}
         <Divider />
       </Row>
     </div>
